48ffdf0af5a64989e32743c6caa5d8aa0dbd88fb
AlienZombieNecro <49724453+AlienZombieNecro@users.noreply.github.com>
2019-07-01 16:13:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,11 +41,8 @@ client.on('message', message1 =>{
 //spam grant
 client.on('message', dmgrant => {
     if (dmgrant.content.startsWith('/dm ') && dmgrant.mentions.users.size) {
-        for (let i = 0; i < 10	; i++) {
 	var v=dmgrant.toString().split(' ').shift().shift().join(' ') // Takes the DM content from the message
         var member=dmgrant.mentions.users[0] // The mentioned user
-	member.send(v) // send that user a DM
-	}
     }
 
 
