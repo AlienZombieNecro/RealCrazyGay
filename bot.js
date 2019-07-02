9ecd9fb4019ba2dd1bc5a0ee77925090d522aafb
AlienZombieNecro <49724453+AlienZombieNecro@users.noreply.github.com>
2019-07-01 21:13:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,13 +33,7 @@ client.on('message', flip2 =>{
 	switch(args[0]){
 		case 'coinflip2':
 		flip2.delete (1000);
-		var number = (Math.floor(Math.random() * 2) == 0);
-		if (number === 1){
-			flip2.channel.send("The coin landed on **HEADS**!")
-		}
-		else {
-			flip2.channel.send("The coin landed on **TAILS**!")
-		}
+		flip2.channel.send(return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';)
 	}
 });
 
