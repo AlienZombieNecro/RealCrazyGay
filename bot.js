8f8a3ab49d6b58de7509ef16a9a4acdbd55b8696
AlienZombieNecro <49724453+AlienZombieNecro@users.noreply.github.com>
2019-07-01 21:35:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,33 +15,37 @@ client.on('message', msg =>{
 	}
 });
 
-//coinflip command
-client.on('message', flip2 =>{
-	let args = flip2.content.substring(PREFIX.length).split(" ");
+//prefix commands
+client.on('message', message =>{
+	let args = message.content.substring(PREFIX.length).split(" ");
 	switch(args[0]){
-		case 'coinflip':
-		flip2.delete (1000);
-		var x = Math.floor(Math.random() * 2);
-		console.log(x);
-		if (x == 0) {
-			flip2.channel.send("The coin landed on **HEADS**!");
-		}
-		else {
-			flip2.channel.send("The coin landed on **TAILS**!");
-		}
-
-
+		case 'coinflip': //coinflip command
+			message.delete (1000);
+			var x = Math.floor(Math.random() * 2);
+			console.log(x);
+			if (x == 0) {
+				message.channel.send("The coin landed on **HEADS**!");
+			}
+			else {
+				message.channel.send("The coin landed on **TAILS**!");
+			}
+		case 'profile'
+			const embed = new Discord.RichEmbed()
+			.addField('Player Name', message.author.username);
+			message.channel.sendEmbed(embed);
+		break;
+//dont touch under
 	}
 });
-
+//dont touch above
 
 
 //monkey command
 client.on('message', monkey =>{
 	if (monkey.content === "@monkey") {
 		monkey.delete (1000);
-		for (let i = 0; i < 10	; i++) {
-			monkey.channel.send('<@244645880446189568>').then(d_msg => {d_msg.delete(3000); });
+		for (let i = 0; i < 5	; i++) {
+			monkey.channel.send('<@244645880446189568>').then(d_msg => {d_msg.delete(10000); });
 		}
 	}
 });
