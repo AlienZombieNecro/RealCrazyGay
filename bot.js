71bf2174e6a6a71efbe0c3571719affc739d7e07
AlienZombieNecro <49724453+AlienZombieNecro@users.noreply.github.com>
2019-07-01 21:21:12 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,10 +35,10 @@ client.on('message', flip2 =>{
 		flip2.delete (1000);
 		var x =Math.floor(Math.random()*2);
 		if ( x==0){
-		flip2.channel.send("Heads");
+		flip2.channel.send("Heads")
 		}
 		else{
-		flip2.channel.send("Tails");
+		flip2.channel.send("Tails")
 		}}
 
 
